c4e3ac3c64c92b02bbca259311f08c467c0c6274
aryanverma-04 <93571501+aryanverma-04@users.noreply.github.com>
2022-10-31 17:58:26 +0530

    Delete admin directory

--- admin/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con = mysqli_connect("localhost","root", "","trip");
-
-if (mysqli_connect_error()) {
-    echo"Cannot connect database";
-}
-
-?>
--- admin/connnectionemployees.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con = mysqli_connect("localhost","root", "","employees");
-
-if (mysqli_connect_error()) {
-    echo"Cannot connect database";
-}
-
-?>
